de47778785c0ef9151ab6e1da9c7f7456749d53b
Saadiq Hamza <hmsaadiq@gmail.com>
2024-08-15 15:06:51 +0100

    another commit 02

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          nothing like Learn Reactx2
         </a>
       </header>
     </div>
