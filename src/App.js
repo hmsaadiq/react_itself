3b30edd193df7be8f01c7faf1a9a5e188bfadd7a
Saadiq Hamza <hmsaadiq@gmail.com>
2024-08-21 14:44:23 +0100

    added variable

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import OurHeader from './components/Header';
 import Cake from './components/Cake';
 import { useState } from 'react';
-
-function App() {
+//this code defines a simple app that renders a header, a cake component with a dynamic size, and three buttons to update the cake size.
+function App() { //The App component is a functional component that returns JSX elements.
   //   const sizes = [100, 200, 400];
-  const [size, setSize] = useState(200);
+  const [size, setSize] = useState(200); //The useState hook is used to create a state variable size and an setSize function to update it. 
 
   return (
-    <>
+    <> 
+     {/* this is a fragment, its needed to keep html type code in place*/}
       <OurHeader />
-      {/* {sizes.map((size) => (
+      {/* {sizes.map((size) => ( //would render multiple Cake components with different sizes using the sizes array.
         <Cake size={size} />
       ))} */}
-      <Cake size={size} />
-      {'The cake is now size' + size}
+      <Cake size={size} /> 
+      {/* A single Cake component is rendered with the current size value as a prop. */}
+      {'The cake is now size ' + size} 
       <button onClick={() => setSize(100)}>Make me 100</button>
       <button onClick={() => setSize(200)}>Make me 200</button>
       <button onClick={() => setSize(400)}>Make me 400</button>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
+const title = "customer A"
 function Header() {
-  return <h1>Welcome to CakeOrder.com</h1>;
+  return (
+    <>
+    <h1>Hello, {title.toUpperCase()}</h1>
+      <h1>Welcome to CakeOrder.com</h1>
+      
+      <button type="button" className="primary"> I want to place an order!</button>
+    </>
+  );
 }
 
 export default Header;
