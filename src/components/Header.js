5487d3e1f682e1a81aa26a1a80a73ea4fc4ec771
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-08-21 15:34:27 +0100

    Cake Order Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,26 @@
 function Header() {
-  return <h1>Welcome to CakeOrder.com</h1>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0px 30px',
+        backgroundColor: 'blue',
+      }}
+    >
+      <img src='/images/cake.webp' width={100} />
+      <div
+        style={{
+          flexGrow: 1,
+          // border: '1px solid black',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <h2>Hello Sadiq, Welcome to CakeOrder.com</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
